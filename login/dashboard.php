0580433a49ff6a1de9d6b45f7b9ebb06a478a0b9
Luke Kalyan Brata Sarker <dhrubo.luke@gmail.com>
2024-11-04 05:10:16 +0600

    Logout is done destroying Session

--- login/dashboard.php
+++ login/dashboard.php
@@ -4,6 +4,10 @@
         $userEmail = $_SESSION['email'];
         $userData = file_get_contents("userdetails.json");
         $userInfo = json_decode($userData,true);
+        if(isset($_POST['logout'])){
+            session_destroy();
+            header("location:index.php");
+        }
         if(isset($_POST['update'])){
             $userInfo[$userEmail]['name'] = $_POST['name'];
             $userInfo[$userEmail]['role'] = $_POST['role'];
@@ -116,11 +120,15 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                         </svg>
                     </button>
-                    <button class="text-gray-500 focus:outline-none focus:text-gray-700">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
-                        </svg>
-                    </button>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+                        <input type="hidden" name="logout">
+                        <button type="submit" class="text-gray-500 focus:outline-none focus:text-gray-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
+                            </svg>
+                        </button>
+                    </form>
+                    
                     <img class="h-8 w-8 rounded-full ml-4" src="https://avatar.iran.liara.run/public/33" alt="User avatar">
                 </div>
             </header>
